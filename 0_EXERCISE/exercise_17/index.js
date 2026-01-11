e3523f88dfd9599561e73a6d36f16ebbe3f3d4b1
Takeuchi04 <kyohei.takeuchi@flinters-base.co.jp>
2026-01-11 11:35:51 +0900

    js string メソッド

---
+++ 0_EXERCISE/exercise_17/index.js
@@ -0,0 +1,10 @@
+// Stringメソッドの演習問題
+// Stringのメソッドを使ってみましょう。
+// messageという変数を用意しました。
+// messageに定義されている値を変えることなく、messageの文字を小文字に変換した値をwhisperという変数に入れてください。
+// また、このときに文字列の両端の空白はすべて取り除いてください。
+const message = "    TASTE THE RAINBOW!  "; //この行は編集しないでください！
+
+// この下にコードを書いてください
+
+const whisper = message.toLowerCase().trim();

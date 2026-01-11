6460e45b4ce36341f7687a05b222cfaaae2b3bac
Takeuchi04 <kyohei.takeuchi@flinters-base.co.jp>
2026-01-11 11:10:28 +0900

    js string演習

---
+++ 0_EXERCISE/exercise_16/index.js
@@ -0,0 +1,7 @@
+// stringの演習問題
+// stringを値にもつ以下の変数を定義してください（let/constどちらで定義しても良いです）：
+// purpleという文字列を持つbestColorという変数
+// You had me at "hello"という文字列を持つquoteという変数（文字列の中でダブルクォート"を含めている点に注意が必要です）
+
+const bestColor= "purple";
+const quote= `You had me at "hello"`;

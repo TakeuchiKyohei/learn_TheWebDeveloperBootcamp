82d85b79fa7206847b804a8aa27b68dcd574c148
Takeuchi04 <kyohei.takeuchi@flinters-base.co.jp>
2026-01-24 21:05:56 +0900

    dom イベントのthisの特性

---
+++ 6_DOM_INTREO/eventAndThis/app.js
@@ -0,0 +1,20 @@
+const createRGB = () => {
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+  return `rgb(${r}, ${g}, ${b})`;
+}
+const buttons = document.querySelectorAll('button');
+const h1s = document.querySelectorAll('h1');
+
+for (let button of buttons) {
+  button.addEventListener("click", colorize);
+}
+for (let h1 of h1s) {
+  h1.addEventListener("click", colorize)
+}
+
+function colorize() {
+  this.style.backgroundColor = createRGB();
+  this.style.color = createRGB();
+}

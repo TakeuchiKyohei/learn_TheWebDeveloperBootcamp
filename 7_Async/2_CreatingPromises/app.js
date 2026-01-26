f3a104e8c1b46c1291955bb8d56229a26c11fbaf
Takeuchi04 <kyohei.takeuchi@flinters-base.co.jp>
2026-01-27 01:48:32 +0900

    async await

--- 7_Async/2_CreatingPromises/app.js
+++ 7_Async/2_CreatingPromises/app.js
@@ -28,10 +28,27 @@ const delayedColorChange = (color, delay) => {
     })
 }
 
-delayedColorChange("red", 1000)
-    .then(() => delayedColorChange("orange", 1000))
-    .then(() => delayedColorChange("yellow", 1000))
-    .then(() => delayedColorChange("green", 1000))
-    .then(() => delayedColorChange("blue", 1000))
-    .then(() => delayedColorChange("indigo", 1000))
-    .then(() => delayedColorChange("violet", 1000))
+// delayedColorChange("red", 1000)
+//     .then(() => delayedColorChange("orange", 1000))
+//     .then(() => delayedColorChange("yellow", 1000))
+//     .then(() => delayedColorChange("green", 1000))
+//     .then(() => delayedColorChange("blue", 1000))
+//     .then(() => delayedColorChange("indigo", 1000))
+//     .then(() => delayedColorChange("violet", 1000))
+
+async function rainbow(){
+    await delayedColorChange("red", 1000);
+    delayedColorChange("orange", 1000);
+}
+
+rainbow();
+
+async function makeReq() {
+    try {
+        let data1 = await fakeReq("/hoge");
+        console.log(data1);
+    }catch(e){
+        console.log("エラーが発生しました", e);
+    }
+}
+makeReq()

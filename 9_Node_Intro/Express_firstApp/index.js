c19a5332af399719d419b5b07ba0c9e5362a9a8b
Takeuchi04 <kyohei.takeuchi@flinters-base.co.jp>
2026-02-01 20:38:39 +0900

    node.js入門 express .get .post

--- 9_Node_Intro/Express_firstApp/index.js
+++ 9_Node_Intro/Express_firstApp/index.js
@@ -2,9 +2,30 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.use((req,res) => {
-  console.log("リクエストきたよ。");
-  res.send("<h1>初めてのExpressレスポンス</h1>");
+// app.use((req,res) => {
+//   console.log("リクエストきたよ。");
+//   res.send("<h1>初めてのExpressレスポンス</h1>");
+// })
+
+app.get("/", (req, res) => {
+  console.log("ルートパスにアクセスがありました。")
+  res.send("<h1>root path</h1>")
+})
+app.get("/cats", (req, res) => {
+  console.log("/catsにアクセスがありました。")
+  res.send("<h1>cats path</h1>")
+})
+app.get("/dogs", (req, res) => {
+  console.log("/dogsにアクセスがありました。")
+  res.send("<h1>dogs path</h1>")
+})
+app.get(/(.*)/, (req, res) => {
+  console.log("*にアクセスがありました。")
+  res.send("<h1>404 Not Found</h1>")
+})
+app.post("/cats", (req, res) => {
+  console.log("/cats(POST)にアクセスがありました。")
+  res.send("<h1>cats(POST) path</h1>")
 })
 
 app.listen(port, () => {

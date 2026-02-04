59902ae21e9dde9af0682eda6c78ca16c3127741
Takeuchi04 <kyohei.takeuchi@flinters-base.co.jp>
2026-02-05 00:34:13 +0900

    node.js express ejs subredditなテンプレdemo

--- 9_Node_Intro/Template_DEMO/index.js
+++ 9_Node_Intro/Template_DEMO/index.js
@@ -13,6 +13,10 @@ app.get('/random', (req, res)=>{
   const num = Math.floor(Math.random() * 10)+1;
   res.render("random", { num });
 })
+app.get('/r/:subreddit', (req, res)=>{
+  const { subreddit } = req.params;
+  res.render("subreddit", { subreddit });
+})
 
 app.listen(PORT,()=>{
   console.log('Server is running');
